e145b295bbd68f0cf1bf7d05ec187c5120a4cf00
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-12-07 20:07:14 +0100

    did the first part

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,25 @@
+import readline from "readline";
+
 export function cleanInput(input: string): string[] {
-    return input.trim().toLowerCase().split(/\s+/)
+  return input.trim().toLowerCase().split(/\s+/);
+}
+
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin, // where user types
+    output: process.stdout, // where you print
+    prompt: "Pokedex > ", // what the user sees as the prompt
+  });
+
+  rl.prompt();
+  rl.on("line", (line) => {
+    const words = cleanInput(line)
+    if (words.length === 0) {
+        rl.prompt()
+        return
+    } 
+
+    console.log(`Your command was: ${words[0]}`)
+    rl.prompt()
+  });
 }
